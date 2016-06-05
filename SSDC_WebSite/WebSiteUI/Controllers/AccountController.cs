4489901d697b50f30afd258be844a5522f4ccb3b
nao14 <nao14@DESKTOP-HLL3SQ7>
2016-06-05 11:09:42 +0300

    Added Admin-Login View/Model

---
+++ SSDC_WebSite/WebSiteUI/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteUI.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+
+        }
+    }
+}
---
+++ SSDC_WebSite/WebSiteUI/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClassLibrary.Abstract;
+
+
+namespace WebSiteUI.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        IPersonRepository personRepo;
+
+        public AdminController(IPersonRepository repo)
+        {
+            personRepo = repo;
+        }
+         
+        public ActionResult Index()
+        {
+            return View(personRepo.People);
+        }
+    }
+}
