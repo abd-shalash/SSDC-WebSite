d4dfe655b94046366eeb9487aed6555b8fb97387
abd-shalash <abd_shalash@hotmail.com>
2016-06-15 13:49:25 +0300

    add 27 db classes

---
+++ SSDC_WebSite/ClassLibrary/Entities/booking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Entities
+{
+    public class booking
+    {
+        public int booking_id { get; set; }
+        public int user_id { get; set; }
+        public DateTime booking_date { get; set; }//should be of type time date 
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
+        public string topic { get; set; }
+        public int no_of_participant { get; set; }
+        public int participant_level_id { get; set; }
+        public int department_id { get; set; }
+        public bool it_support { get; set; }
+        public string code_no { get; set; }
+        public string booking_status { get; set; }
+        public int status_updated_by { get; set; }
+
+    }
+}
---
+++ SSDC_WebSite/ClassLibrary/Entities/form.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Entities
+{
+   public class form
+    {
+        public int form_id { get; set; }
+        public string form_name { get; set; }
+    }
+}
