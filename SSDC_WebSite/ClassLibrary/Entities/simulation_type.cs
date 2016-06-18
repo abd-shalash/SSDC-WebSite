2b9a6c3085dc71c9c55bbefa757a55195b354de9
nao1422 <nao1422@gmail.com>
2016-06-18 18:20:53 +0300

    Entities changes

--- SSDC_WebSite/ClassLibrary/Entities/simulation_type.cs
+++ SSDC_WebSite/ClassLibrary/Entities/simulation_type.cs
@@ -9,7 +9,8 @@ namespace ClassLibrary.Entities
     class simulation_type
     {
         public int simulation_type_id { get; set; }
-        public string field_name { get; set; }
+        public string simulation_type_name { get; set; }
+        public string simulation_type_desc { get; set; }
         public string meeting_status {get; set; }
     }
 }
