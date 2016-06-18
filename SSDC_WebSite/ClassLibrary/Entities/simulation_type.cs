6f2c8027887e820f10560907c26db41a05643528
abd-shalash <abd_shalash@hotmail.com>
2016-06-18 23:42:49 +0300

    asd

---
+++ SSDC_WebSite/ClassLibrary/Entities/simulation_type.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Entities
+{
+    class simulation_type
+    {
+        public int simulation_type_id { get; set; }
+        public string simulation_type_name { get; set; }
+        public string simulation_type_desc { get; set; }
+        public string meeting_status {get; set; }
+    }
+}
---
+++ SSDC_WebSite/WebSiteUI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult index()
+        {
+            return View();
+        }
+
+    }
+}
