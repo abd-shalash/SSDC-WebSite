5d6c4e9c1945e2b686cdd0d3dd469898b6d6e7c4
nao1422 <nao1422@gmail.com>
2016-06-18 22:04:45 +0300

    Cleanup

--- SSDC_WebSite/ClassLibrary/Entities/booking_item.cs
+++ SSDC_WebSite/ClassLibrary/Entities/booking_item.cs
@@ -8,13 +8,9 @@ namespace ClassLibrary.Entities
 {
     class booking_item
     {
-<<<<<<< HEAD
-        
-=======
         public int booking_item_id { get; set; }
         public int booking_id { get; set; }
         public int item_item_id { get; set; }
         public string item_status { get; set; }
->>>>>>> 7ad1d65ec373e1ff0f18ed4a168b96f667b18c16
     }
 }
