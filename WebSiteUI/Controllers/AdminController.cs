e01d515176a9e5ae8b6e0a283e0140c43118ae89
abdu red <abdu red@DESKTOP-L2OQQUC>
2016-08-09 16:07:54 +0300

    db v7

--- WebSiteUI/Controllers/AdminController.cs
+++ WebSiteUI/Controllers/AdminController.cs
@@ -3,10 +3,12 @@ using ClassLibrary.Concrate;
 using SSDC_WebSite.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using WebSiteUI.Models;
 
 namespace WebSiteUI.Controllers
 {
@@ -32,7 +34,86 @@ namespace WebSiteUI.Controllers
         }
         public ActionResult CreateUser()
         {
-            return View();
+            var model = new CreateUserModel();
+            return View(model);
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult CreateUser(CreateUserModel model)
+        {
+
+
+            position tempPos = new position();
+            tempPos = repository.positions.FirstOrDefault(p => p.position_id == model.PositionID);
+
+            department tempDep = new department();
+            tempDep = repository.departments.FirstOrDefault(p => p.department_id == model.DepartmentID);
+
+            organization tempOrg = new organization();
+            tempOrg = repository.organizations.FirstOrDefault(p => p.organization_id == model.OrganizationID);
+            user newuser = new user
+            {
+                email = model.Email,
+                first_name = model.Fname,
+                mobile_number = model.phone_number,
+                last_name = model.Lname,
+                password = model.Password,
+                gender = model.Gender,
+                organization = tempOrg,
+                //new organization
+                //{
+                //    organization_id = tempOrg.organization_id,
+                //    organization_name = tempOrg.organization_name,
+                //    organization_type = tempOrg.organization_type
+
+                //},
+                department = tempDep,
+                //new department
+                //{
+                //    department_id = tempDep.department_id,
+                //    department_name = tempDep.department_name,
+                //    organization_type = tempDep.organization_type
+
+                //},
+
+                position = tempPos,
+                //new position
+                //{
+                //    position_id = tempPos.position_id,
+                //    position_name = tempPos.position_name
+                //}
+            };
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //  bool created = repository.addUser(newuser);
+                    var tempBool = repository.users.FirstOrDefault(i => i.email == newuser.email);
+                    if (tempBool != null)
+                    {
+                        ModelState.AddModelError("", "ERROR a user with that email exsist ");
+                        return View(model);
+                    }
+
+
+                    repository.users.Add(newuser);
+                    //repository.users.Local.savesave();
+                    repository.SaveChanges();
+                    return View(model);
+
+
+                }
+                else { ModelState.AddModelError("", "error make sure that you have filled all the text boxes "); }
+            }
+            catch (DataException /* dex */)
+            {
+
+                ModelState.AddModelError("", "error  ");
+
+
+            }
+
+            return View(model);
         }
         public ViewResult EditUser(int? id)
         {
@@ -74,10 +155,18 @@ namespace WebSiteUI.Controllers
             return View(student);
          
         }
-        [HttpPost]
+        
         public ActionResult DeleteUser(int? id)
         {
+            if (id== null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
             user Deleteduser = repository.users.Find(id);
+            if (Deleteduser== null)
+            {
+                return HttpNotFound();
+            }
             if (Deleteduser!=null)
             {
                 repository.users.Remove(repository.users.Find(id));
