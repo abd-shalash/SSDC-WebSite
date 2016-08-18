a2f0a86ff1fd87f7ab96fe5c5fadc7f2f37f2e95
rwepnu <rehabfahad.m@gmail.com>
2016-08-18 12:15:21 +0300

    layla

--- ClassLibrary/Migrations/201608160704036_one.Designer.cs
+++ ClassLibrary/Migrations/201608160704036_one.Designer.cs
@@ -7,13 +7,13 @@ namespace ClassLibrary.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class first : IMigrationMetadata
+    public sealed partial class one : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(first));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(one));
         
         string IMigrationMetadata.Id
         {
-            get { return "201608101842372_first"; }
+            get { return "201608160704036_one"; }
         }
         
         string IMigrationMetadata.Source
--- ClassLibrary/Migrations/201608160704036_one.cs
+++ ClassLibrary/Migrations/201608160704036_one.cs
@@ -3,7 +3,7 @@ namespace ClassLibrary.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class first : DbMigration
+    public partial class one : DbMigration
     {
         public override void Up()
         {
